285cdcee2f50e7e6b2256d98431055d59bc67304
camilod29 <cadelgado@uniboyaca.edu.co>
2022-10-30 22:48:00 -0500

    malditos headers no puedo con el POST

--- app/src/main/java/com/example/consumirapi/MainActivity.java
+++ app/src/main/java/com/example/consumirapi/MainActivity.java
@@ -40,26 +40,44 @@ public class MainActivity extends AppCompatActivity {
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LeerWs();
-                enviarWs(txtTitle.getText().toString(), txtBody.getText().toString(), txtUser.getText().toString());
-                actualizarWs(txtTitle.getText().toString(), txtBody.getText().toString(), txtUser.getText().toString());
-                eliminarWs();
+                //LeerWs();
+                //enviarWs("nuevito","nuevito69","456","novato");
+                //enviarWs(txtTitle.getText().toString(), txtBody.getText().toString(), txtUser.getText().toString());
+                //actualizarWs(txtTitle.getText().toString(), txtBody.getText().toString(), txtUser.getText().toString());
+                //eliminarWs();
+                consumirServicio();
             }
         });
     }
+
+    private void consumirServicio() {
+        // ahora ejecutaremos el hilo creado
+        String names= "nuevo";
+        String username= "nuevin66";
+        String password= "23455";
+        String rol="user";
+
+
+        ServicioTask servicioTask= new ServicioTask(this,"https://30df-186-144-129-49.ngrok.io/api/users",names,username,password,rol);
+        servicioTask.execute();
+
+
+    }
+
     private void LeerWs() {
 
-        String url = "https://jsonplaceholder.typicode.com/posts/11";
-
+        //String url = "https://jsonplaceholder.typicode.com/posts/1";
+        //String url="http://192.168.4.1:8080/api/users";
+        String url ="https://30df-186-144-129-49.ngrok.io/api/users";
         StringRequest postResquest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
                     JSONObject jsonObject = new JSONObject(response);
-                    txtUser.setText(jsonObject.getString("userId"));
-                    String title = jsonObject.getString("title");
+                    txtUser.setText(jsonObject.getString("data"));
+                    String title = jsonObject.getString("date");
                     txtTitle.setText(title);
-                    txtBody.setText(jsonObject.getString("body"));
+                    txtBody.setText(jsonObject.getString("rol"));
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -74,9 +92,9 @@ public class MainActivity extends AppCompatActivity {
         Volley.newRequestQueue(this).add(postResquest);
     }
 
-    private void enviarWs(final String title, final String body, final String userId) {
+    private void enviarWs(final String names, final String username, final String password,final String rol) {
 
-        String url = "https://jsonplaceholder.typicode.com/posts";
+        String url = "https://30df-186-144-129-49.ngrok.io/api/users";
 
         StringRequest postResquest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
@@ -86,16 +104,26 @@ public class MainActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e("Error", error.getMessage());
+                try {
+                    Log.e("Error", error.getMessage());
+                }catch (Exception e){
+
+                }
             }
         }) {
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
-                params.put("title", title);
-                params.put("body", body);
-                params.put("userId", userId);
+                params.put("names", names);
+                params.put("username", username);
+                params.put("password", password);
+                params.put("rol", rol);
+
 
                 return params;
+                /*"names": "TerceroHugo",
+    "username": "Hugo05",
+    "password": "12345",
+    "rol": "user"*/
             }
         };
         Volley.newRequestQueue(this).add(postResquest);
@@ -132,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void eliminarWs() {
 
-        String url = "https://jsonplaceholder.typicode.com/posts/1";
+        String url = "https://30df-186-144-129-49.ngrok.io/api/users/2";
 
         StringRequest postResquest = new StringRequest(Request.Method.DELETE, url, new Response.Listener<String>() {
             @Override
